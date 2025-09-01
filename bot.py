982b99f31b3671c5265a255993c91c8c4b35a7c6
Esha Susan Shaji <eshasusanshaji@gmail.com>
2025-09-01 09:57:17 +0530

    Use an existing api to get memes from Reddit

--- bot.py
+++ bot.py
@@ -1,9 +1,13 @@
 import discord
 import os
 from dotenv import load_dotenv
-
+import requests
+import json
 load_dotenv()
-
+def get_meme():
+    response=requests.get('https://meme-api.com/gimme')
+    json_data=json.loads(response.text)
+    return json_data['url']
     
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -11,8 +15,9 @@ class MyClient(discord.Client):
     async def on_message(self,message):
         if message.author==self.user:
             return
-        if message.content.startswith('$hello'):
-            await message.channel.send('Hello World')
+        if message.content.startswith('$meme'):
+            await message.channel.send(get_meme())
+           
 intents=discord.Intents.default()
 intents.message_content=True
 client =MyClient(intents=intents)
