645a9b65bb14811840cd4f7366d0ad30c9c8aab2
Esha Susan Shaji <eshasusanshaji@gmail.com>
2025-09-01 09:35:56 +0530

    add logic to respond to a simple hello message

--- bot.py
+++ bot.py
@@ -1,12 +1,18 @@
 import discord
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
+    
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged in as {0}!'.format(self.user))
-
+    async def on_message(self,message):
+        if message.author==self.user:
+            return
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello World')
 intents=discord.Intents.default()
 intents.message_content=True
 client =MyClient(intents=intents)
